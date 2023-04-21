a55a6e8b48567c53c4613d19207a170575ddb720
Saman <pradhansaman2017@gmail.com>
2023-04-21 20:19:12 +0530

    navbar structure

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,5 +1,15 @@
 /* show menu */
+let navMenu = document.getElementById("nav-menu");
+let navToggle = document.getElementById("nav-toggle");
+let navClose = document.getElementById("nav-close");
 
+// menu showing
+// validate if the toggle here\
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 /* remove menu mobile */
 
 /* add blur to header  */
