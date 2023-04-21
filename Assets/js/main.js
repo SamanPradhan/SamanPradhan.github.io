2be4331e65fbdeecf7f36c6c9209254a3713bea2
Saman <pradhansaman2017@gmail.com>
2023-04-21 21:56:34 +0530

    navbar responsive

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -8,10 +8,29 @@ let navClose = document.getElementById("nav-close");
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
+    console.log("role");
+  });
+}
+
+//menu hiding
+//validate if the toggle here
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+    console.log("role");
   });
 }
 /* remove menu mobile */
+let navLink = document.querySelectorAll(".nav--link");
 
+let linkhere = () => {
+  let navMenu = document.getElementById("nav-menu");
+  //when click on nav--link, we remove the show-menu class
+
+  navMenu.classList.remove("show-menu");
+
+  navLink.forEach((n) => n.addEventListener("click", linkhere));
+};
 /* add blur to header  */
 
 /* email js */
